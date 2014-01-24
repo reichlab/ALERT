74804c2e5c89bc06f30ecc3d8abd0037dfc9bf9b
stephenalex913 <stephenalauer@gmail.com>
2014-01-24 13:56:06 -0500

    Added maxDuration element to evalALERT and robustALERT (formerly multiruleALERT).
    Incorporated changes into vignette, though this is still a work in progress.
    
    Additionally, vignette references .rda file that needs to be masked before added to package.

--- R/ALERT.R
+++ R/ALERT.R
@@ -15,7 +15,7 @@ require(lubridate)
 
 #' 
 #' @return A matrix summarizing the performance of different ALERT thresholds on the given data. Each row represents a threshold. The columns correspond to (1) threshold used, (2) mean ALERT duration, (3/4/5/6) mean/minimum/maximum/sd of % of cases captured across seasons.
-createALERT <- function(data, firstMonth=9, lag=7, minWeeks=8, allThresholds=TRUE, k, target.pct=NULL) {
+createALERT <- function(data, firstMonth=9, lag=7, minWeeks=8, allThresholds=TRUE, k=0, target.pct=NULL) {
     ## check for correct column headers
     if( !("Date" %in% colnames(data)) | !("Cases" %in% colnames(data)) )
         stop("data needs Date and Cases columns.")
@@ -94,7 +94,7 @@ createALERT <- function(data, firstMonth=9, lag=7, minWeeks=8, allThresholds=TRU
 #'      [5] 1 of peak was captured, 0 otherwise, 
 #'      [6] 1 if peak +/- k weeks captured, 0 otherwise, 
 #'      [7] 1 if counts rise above threshold for two consecutive weeks at any point after ALERT period, 0 otherwise. 
-applyALERT <- function(data, threshold, k=NULL, lag, minWeeks, target.pct=NULL, plot=FALSE) {
+applyALERT <- function(data, threshold, k=0, lag, minWeeks, target.pct=NULL, plot=FALSE) {
     ## find first week where ALERT is hit
     idxHitDate <- min(which(data$Cases>=threshold))
     hitDate <- data[idxHitDate,"Date"]
@@ -145,7 +145,8 @@ applyALERT <- function(data, threshold, k=NULL, lag, minWeeks, target.pct=NULL, 
     out["peak.captured"] <- idxPeak>=idxStartDate & idxPeak<=idxEndDate ## peak captured
     out["peak.ext.captured"] <- idxPeak>=(idxStartDate+k) & idxPeak<=(idxEndDate-k) ## peak +/- k weeks captured
     out["low.weeks.incl"]  <- sum(data[idxStartDate:idxEndDate, "Cases"] < threshold)
-    if(!is.null(target.pct)) out <- c(out, duration.diff=unname(out["duration"]-postcast["duration"])) 
+    if(!is.null(target.pct)) out <- c(out, duration.diff=unname(out["duration"]-postcast["duration"]))
+    else out <- c(out, duration.diff=NA)
     if(plot) message("Plot option not implemented.")
     
     return(out)
@@ -158,10 +159,14 @@ applyALERT <- function(data, threshold, k=NULL, lag, minWeeks, target.pct=NULL, 
 #' @param firstMonth month number which is counted as the first month of the 'flu year' 
 #' @param lag lag time between report date and action taken
 #' @param minWeeks minimum number of weeks to be in ALERT
-#' @param minPercent specify the minimum percent of cases to be captured by ALERT. This enables automated threshold selection.
+#' @param target.pct optional, can specify the percentage of cases the user is targeting during the ALERT period when testing maxDuration
 #' @param allThresholds If TRUE, all integer threshold values between the 10th and 50th percentile are examined. If FALSE, only the 10th, 20th, 30th, 40th, and 50th percentiles are examined.
 #' @param k the number of weeks around the peak to evaluate ALERT coverage for
-evalALERT <- function(data, firstMonth, lag, minWeeks, minPercent, allThresholds, k) {
+#' @param minPercent specify the minimum percent of cases to be captured on average by ALERT. This enables automated threshold selection.
+#' @param maxDuration specify the maximum number of weeks to be captured on average by ALERT. This enables automated threshold selection.
+evalALERT <- function(data, firstMonth, lag, minWeeks, target.pct=NULL, allThresholds, k=0, minPercent=NULL, maxDuration=NULL) {
+    if(is.null(maxDuration) & is.null(minPercent))
+        stop("Please choose a rule to evaluate, either with maxDuration or minPercent.")
     ## check for correct column headers
     if( !("Date" %in% colnames(data)) | !("Cases" %in% colnames(data)) )
         stop("Data needs Date and Cases columns.")
@@ -178,33 +183,58 @@ evalALERT <- function(data, firstMonth, lag, minWeeks, minPercent, allThresholds
         endDate <- as.Date(paste0(years[i]+1, "-", firstMonth, "-01"))
         idxs[[i]] <- which(data$Date >= startDate & data$Date < endDate)   
     }
-    ## for each season, run createALERT on other data, the applyALERT to the left out year. 
-    for(j in 1:length(years)){
-        if(length(idxs[[j]])<=3*minWeeks) next # if a season is too short to evaluate, skip it
-        ## leave one season out of data
-        data1 <- data[-idxs[[j]],]
-        ## run createALERT on other data
-        output <- as.data.frame(createALERT(data=data1, firstMonth=firstMonth, lag=lag, minWeeks=minWeeks, allThresholds=allThresholds, k=k, target.pct=minPercent)$out)
-        output1 <- subset(output, mean.pct.cases.captured>minPercent)
-        ## find largest threshold that achieves target percent covered
-        if(length(output1[,1])==0) next # skips any years where target percentage is not achieved
-        opt.thresh <- max(output1$threshold)
-        ## run applyALERT on left out year with threshold determined above
-        if(max(data[idxs[[j]],2])<opt.thresh) next # skips any years where threshold is not achieved (usually partial season at beginning or end of data)
-        aaa <- applyALERT(data[idxs[[j]],], threshold=opt.thresh, k=k, lag=lag, 
-                          minWeeks=minWeeks, target.pct=minPercent, plot=FALSE)
-        ## store metrics
-        aaa <- c(year=years[j], threshold=opt.thresh, aaa)
-        eval.dat <- rbind.data.frame(eval.dat, aaa)
+    ## for each season, run createALERT on other data, the applyALERT to the left out year.
+    if(!is.null(minPercent)){
+        for(j in 1:length(years)){
+            if(length(idxs[[j]])<=3*minWeeks) next # if a season is too short to evaluate, skip it
+            ## leave one season out of data
+            data1 <- data[-idxs[[j]],]
+            ## run createALERT on other data
+            output <- as.data.frame(createALERT(data=data1, firstMonth=firstMonth, lag=lag, minWeeks=minWeeks, allThresholds=allThresholds, k=k, target.pct=minPercent)$out)
+            output1 <- subset(output, mean.pct.cases.captured>minPercent)
+            ## find largest threshold that achieves target percent covered
+            if(length(output1[,1])==0) next # skips any years where target percentage is not achieved
+            opt.thresh <- max(output1$threshold)
+            ## run applyALERT on left out year with threshold determined above
+            if(max(data[idxs[[j]],2])<opt.thresh) next # skips any years where threshold is not achieved (usually partial season at beginning or end of data)
+            aaa <- applyALERT(data[idxs[[j]],], threshold=opt.thresh, k=k, lag=lag, 
+                              minWeeks=minWeeks, target.pct=minPercent, plot=FALSE)
+            ## store metrics
+            aaa <- c(year=years[j], threshold=opt.thresh, aaa)
+            eval.dat <- rbind.data.frame(eval.dat, aaa)
+        }
+        colnames(eval.dat) <- names(aaa)
     }
-    colnames(eval.dat) <- names(aaa)
+    
+    if(!is.null(maxDuration)){
+        for(j in 1:length(years)){
+            if(length(idxs[[j]])<=3*minWeeks) next # if a season is too short to evaluate, skip it
+            ## leave one season out of data
+            data1 <- data[-idxs[[j]],]
+            ## run createALERT on other data
+            output <- as.data.frame(createALERT(data=data1, firstMonth=firstMonth, lag=lag, minWeeks=minWeeks, allThresholds=allThresholds, k=k, target.pct=target.pct)$out)
+            output1 <- subset(output, mean.dur<maxDuration)
+            ## find smallest threshold that has a duration shorter than maxDuration
+            if(length(output1[,1])==0) next # skips any years where all durations are too long
+            opt.thresh <- min(output1$threshold)
+            ## run applyALERT on left out year with threshold determined above
+            if(max(data[idxs[[j]],2])<opt.thresh) next # skips any years where threshold is not achieved (usually partial season at beginning or end of data)
+            aaa <- applyALERT(data[idxs[[j]],], threshold=opt.thresh, k=k, lag=lag, 
+                              minWeeks=minWeeks, target.pct=target.pct, plot=FALSE)
+            ## store metrics
+            aaa <- c(year=years[j], threshold=opt.thresh, aaa)
+            eval.dat <- rbind.data.frame(eval.dat, aaa)
+        }
+        colnames(eval.dat) <- names(aaa)
+    }
+    
     ## take averages of the metrics for final row
     bbb <- c("Mean", mean(eval.dat$threshold), mean(eval.dat$tot.cases), mean(eval.dat$duration), mean(eval.dat$ALERT.cases), mean(eval.dat$ALERT.cases.pct), mean(eval.dat$peak.captured), mean(eval.dat$peak.ext.captured), mean(eval.dat$low.weeks.incl), mean(eval.dat$duration.diff))
     eval.dat <- rbind.data.frame(eval.dat, bbb)
     return(eval.dat)
 }
 
-#' The multiruleALERT function uses evalALERT on a vector of rules to help determine each 
+#' The robustALERT function uses evalALERT on a vector of rules to help determine each 
 #'
 #' @param data the historical data to use in the analysis
 #' @param firstMonth month number which is counted as the first month of the 'flu year' 
@@ -213,22 +243,43 @@ evalALERT <- function(data, firstMonth, lag, minWeeks, minPercent, allThresholds
 #' @param minPercent vector that specifies the minimum percent of cases to be captured by ALERT. This enables automated threshold selection.
 #' @param allThresholds If TRUE, all integer threshold values between the 10th and 50th percentile are examined. If FALSE, only the 10th, 20th, 30th, 40th, and 50th percentiles are examined.
 #' @param k the number of weeks around the peak to evaluate ALERT coverage for
-multiruleALERT <- function(data, firstMonth, lag, minWeeks, minPercent, allThresholds, k) {
+robustALERT <- function(data, firstMonth, lag, minWeeks, target.pct=NULL, allThresholds, k=0, minPercent=NULL, maxDuration=NULL) {
     ## check for correct column headers
     if( !("Date" %in% colnames(data)) | !("Cases" %in% colnames(data)) )
         stop("Data needs Date and Cases columns.")
     
     ## create test matrix
-    mr.dat <- matrix(data=0, nrow=0, ncol=10)
-    for(i in 1:length(minPercent)){
-        one.rule <- evalALERT(data, firstMonth, lag, minWeeks, minPercent[i], 
-                         allThresholds, k)
-        aaa <- one.rule[length(one.rule[,1]),]
-        aaa[1,1] <- minPercent[i]
-        colnames(aaa)[1] <- "minPercent"
-        mr.dat <- rbind.data.frame(mr.dat,aaa)
+    robust.dat <- matrix(data=0, nrow=0, ncol=10)
+    
+    ## run evalALERT on all minPercent rules
+    if(!is.null(minPercent)){
+        for(i in 1:length(minPercent)){
+            one.rule <- evalALERT(data=data, firstMonth=firstMonth, lag=lag, 
+                                  minWeeks=minWeeks,allThresholds=allThresholds, k=k,
+                                  minPercent=minPercent[i])
+            aaa <- one.rule[length(one.rule[,1]),]
+            aaa[1,1] <- paste("minPercent =", minPercent[i])
+            colnames(aaa)[1] <- "rule"
+            #aaa <- cbind.data.frame(aaa[,1], num.years.used=length(one.rule[,1])-1, aaa[,2:10])
+            robust.dat <- rbind.data.frame(robust.dat,aaa)
+        }
     }
-    return(mr.dat)
+    
+    ## run evalALERT on all maxDuration rules
+    if(!is.null(maxDuration)){
+        for(j in 1:length(maxDuration)){
+            one.rule <- evalALERT(data=data, firstMonth=firstMonth, lag=lag, 
+                                  minWeeks=minWeeks,allThresholds=allThresholds, k=k,
+                                  target.pct=target.pct, maxDuration=maxDuration[j])
+            aaa <- one.rule[length(one.rule[,1]),]
+            aaa[1,1] <- paste("maxDuration =", maxDuration[j])
+            colnames(aaa)[1] <- "rule"
+            #aaa <- cbind.data.frame(aaa[,1], num.years.used=length(one.rule[,1])-1, aaa[,2:10])
+            robust.dat <- rbind.data.frame(robust.dat,aaa)
+        }
+    }
+    robust.dat[,6] <- round(as.numeric(robust.dat[,6]),3)
+    return(robust.dat)
 }
 
 #' The postcastALERT function is meant to be called from within the applyALERT function and is used to find the optimal window of time that contains a given percentage of cases.
